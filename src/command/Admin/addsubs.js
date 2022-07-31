beb63f21b8e8181c0f2045fa40cde5f54accf038
PaizTralala <64535433+PaizTralala@users.noreply.github.com>
2022-07-31 15:58:50 +0700

    pretty code & pretty embed

--- src/command/Admin/addsubs.js
+++ src/command/Admin/addsubs.js
@@ -1,62 +1,98 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
-    name: 'addsubs',
-    description: 'Add subscription to user',
-    usage: '<prefix>addsubs <@user> [days]',
-    examples: ['addsubs @Ritshu#0228 31'],
-    aliases: ['addsub'],
-    dir: "Admin",
-    cooldown: 1,
-    permissions: [],
+	name: 'addsubs',
+	description: 'Add subscription to user',
+	usage: '<prefix>addsubs <@user> [days]',
+	examples: ['addsubs @Ritshu#0228 31'],
+	aliases: ['addsub'],
+	dir: 'Admin',
+	cooldown: 1,
+	permissions: [],
 
-    run: async (client, message, args) => {
+	run: async (client, message, args) => {
+		const embed = new MessageEmbed().setColor('#9BEEFF').setFooter({
+			text: 'Powered by GrowZone',
+			iconURL: 'https://cdn.discordapp.com/icons/857396459392729099/a_5f4d3d9a43559fef37d5f20858fef434.gif',
+		});
 
-        if (message.member.roles.cache.find(x => x.name === "Administrator") || message.member.roles.cache.find(x => x.name === "President") || message.member.roles.cache.find(x => x.name === "🔑")) {
-            let user = args[0];
-            user = getMember(message, args[0]).user;
-            let guild = client.guilds.cache.get(client.config.serverID);
-            const premiumRole = await guild.roles.fetch(client.config.premiumID);
-            if (!user) return;
+		if (
+			message.member.roles.cache.find((x) => x.name === 'Administrator') ||
+			message.member.roles.cache.find((x) => x.name === 'President') ||
+			message.member.roles.cache.find((x) => x.name === '🔑')
+		) {
+			let user = args[0];
+			user = getMember(message, args[0]).user;
+			let guild = client.guilds.cache.get(client.config.serverID);
+			const premiumRole = await guild.roles.fetch(client.config.premiumID);
+			if (!user) return;
 
-            let duration = args[1];
-            if (isNaN(duration)) return message.channel.send("invalid duration");
+			let duration = args[1];
+			if (isNaN(duration))
+				return message.reply({
+					embeds: [
+						embed
+							.setAuthor({ name: 'Add Subscription Status: FAILED', iconURL: user.displayAvatarURL({ dynamic: true }) })
+							.setDescription('Please provide a valid duration for the given user!'),
+					],
+				});
 
-            const member = await guild.members.fetch(user.id);
-            if (user && duration) {
-                let durationMS = duration * 86400000;
-                let durationToDB = Date.now() + durationMS;
-                let exist = await client.db.get(`premiumUser.${user.id}`);
-                if (exist) {
-                    let addSubs = durationMS;
-                    await client.db.add(`premiumUser.${user.id}`, addSubs);
-                    message.channel.send(`Succesfully added \`${duration} Days\` of premium subscription to **${user.tag}**`);
-                } else {
-                    await client.db.add(`premiumUser.${user.id}`, durationToDB);
-                    message.channel.send(`Succesfully added \`${duration} Days\` of premium subscription to **${user.tag}**`);
-                    member.roles.add(premiumRole);
-                }
-            }
+			const member = await guild.members.fetch(user.id);
+			if (user && duration) {
+				let durationMS = duration * 86400000;
+				let durationToDB = Date.now() + durationMS;
+				let exist = await client.db.get(`premiumUser.${user.id}`);
 
-            function getMember(message, toFind = '') {
-                toFind = toFind.toLowerCase();
-                let target = message.guild.members.cache.get(toFind);
+				if (exist) {
+					let addSubs = durationMS;
+					const totalSubDate = await client.db.add(`premiumUser.${user.id}`, addSubs);
+					const totalSubDateFormatted = Math.ceil(Object.values(totalSubDate) / 1000);
 
-                if (!target && message.mentions.members) target = message.mentions.members.first();
+					message.reply({
+						embeds: [
+							embed
+								.setAuthor({ name: 'Add Subscription Status: SUCCESS', iconURL: user.displayAvatarURL({ dynamic: true }) })
+								.setDescription(`Succesfully added \`${duration} Days\` more of premium subscription to **${user.tag}**`)
+								.addFields([
+									{
+										name: 'Total premium duration',
+										value: `<t:${totalSubDateFormatted}:R> - <t:${totalSubDateFormatted}>`,
+									},
+								]),
+						],
+					});
+				} else {
+					await client.db.add(`premiumUser.${user.id}`, durationToDB);
 
-                if (!target && toFind) {
-                    target = message.guild.members.cache.find(member => {
-                        return (
-                            member.displayName.toLowerCase().includes(toFind) ||
-                            member.user.tag.toLowerCase().includes(toFind)
-                        );
-                    });
-                }
+					message.reply({
+						embeds: [
+							embed
+								.setAuthor({ name: 'Add Subscription Status: SUCCESS', iconURL: user.displayAvatarURL({ dynamic: true }) })
+								.setDescription(`Succesfully added \`${duration} Days\` of premium subscription to **${user.tag}**`),
+						],
+					});
 
-                // invalid user given
-                if (!target) return message.channel.send(`Please provide a valid user to add premium subscription`);
+					member.roles.add(premiumRole);
+				}
+			}
 
-                return target;
-            }
+			function getMember(message, toFind = '') {
+				toFind = toFind.toLowerCase();
+				let target = message.guild.members.cache.get(toFind);
 
-        }
-    }
-}
+				if (!target && message.mentions.members) target = message.mentions.members.first();
+
+				if (!target && toFind) {
+					target = message.guild.members.cache.find((member) => {
+						return member.displayName.toLowerCase().includes(toFind) || member.user.tag.toLowerCase().includes(toFind);
+					});
+				}
+
+				// invalid user given
+				if (!target) return message.reply(`Please provide a valid user to add premium subscription`);
+
+				return target;
+			}
+		}
+	},
+};
--- src/event/ready.js
+++ src/event/ready.js
@@ -1,96 +1,104 @@
-const { prefix, serverId, premiumId } = require("../../config");
+const { prefix, serverId, premiumId } = require('../../config');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client) => {
-    client.logger.info(`[!] ${client.user.username} is now started...`);
-    client.logger.info(`[!] The bot have ${client.commands.size} commands and ${client.slash.size} (/) commands`);
-    client.user.setActivity(`14/15`, { type: 'PLAYING' });
+	client.logger.info(`[!] ${client.user.username} is now started...`);
+	client.logger.info(`[!] The bot have ${client.commands.size} commands and ${client.slash.size} (/) commands`);
+	client.user.setActivity(`14/15`, { type: 'PLAYING' });
 
+	const embed = new MessageEmbed().setColor('#9BEEFF').setFooter({
+		text: 'Powered by GrowZone',
+		iconURL: 'https://cdn.discordapp.com/icons/857396459392729099/a_5f4d3d9a43559fef37d5f20858fef434.gif',
+	});
 
-    setInterval(async function () {
-        // AUTO PREMIUM ROLE
-        let premiumUser = await client.db.get(`premiumUser`);
-        if (!premiumUser || premiumUser === null) return;
-        const expirationTime = Object.values(premiumUser); // return ms date (arr)
-        const userID = Object.keys(premiumUser); // return userID prem user (arr)
-        const premEntries = Object.entries(premiumUser); // return entries in array
+	setInterval(async function () {
+		// AUTO PREMIUM ROLE
+		let premiumUser = await client.db.get(`premiumUser`);
+		if (!premiumUser || premiumUser === null) return;
+		const expirationTime = Object.values(premiumUser); // return ms date (arr)
+		const userID = Object.keys(premiumUser); // return userID prem user (arr)
+		const premEntries = Object.entries(premiumUser); // return entries in array
 
-        let guild = client.guilds.cache.get(client.config.serverID);
-        const premiumRole = await guild.roles.fetch(client.config.premiumID);
-        let userInRole = guild.roles.cache.get(client.config.premiumID).members.map(x => x.user.id);
-        let nowDateMS = Date.now();
-        // Check if user has prem and expirationTime
+		let guild = client.guilds.cache.get(client.config.serverID);
+		const premiumRole = await guild.roles.fetch(client.config.premiumID);
+		let userInRole = guild.roles.cache.get(client.config.premiumID).members.map((x) => x.user.id);
+		let nowDateMS = Date.now();
+		// Check if user has prem and expirationTime
 
-        if (userInRole.length > 0) {
-            await Promise.all(
-                userInRole.map(
-                    async (user) => {
-                        const member = await guild.members.fetch(user);
-                        let subscription = await client.db.get(`premiumUser.${user}`);
-                        if (!subscription || subscription === null) {
-                            member.roles.remove(premiumRole);
-                            let logsChannel = client.channels.cache.get(client.config.logsID);
-                            logsChannel.send({
-                                embeds: [{
-                                    title: `Premium Role Removal`,
-                                    description: `Removed premium role from **${member.user.tag}** \`[Reason: user doesn't have active subscription]\``,
-                                    color: `#FF9300`,
-                                    footer: {
-                                        text: 'Powered by GrowZone',
-                                        icon_url: 'https://cdn.discordapp.com/icons/857396459392729099/a_5f4d3d9a43559fef37d5f20858fef434.gif',
-                                    }
-                                }]
-                            });
-                        }
-                        if (subscription <= nowDateMS) {
-                            await client.db.delete(`premiumUser.${user}`);
-                            member.roles.remove(premiumRole);
-                            client.logger.database(`Deleted ${member.user.tag} from premiumUser [Reason: Having expired subscription]`);
-                            let logsChannel = client.channels.cache.get(client.config.logsID);
-                            logsChannel.send({
-                                embeds: [{
-                                    title: `Premium Deletion`,
-                                    description: `Deleted **${member.user.tag}** from premiumUser \`[Reason: Having expired subscription]\``,
-                                    color: `#FF9300`,
-                                    footer: {
-                                        text: 'Powered by GrowZone',
-                                        icon_url: 'https://cdn.discordapp.com/icons/857396459392729099/a_5f4d3d9a43559fef37d5f20858fef434.gif',
-                                    }
-                                }]
-                            });
-                        }
-                    }
-                )
-            )
-        }
-        if (userID.length > 0) {
-            await Promise.all(
-                userID.map(
-                    async (user) => {
-                        const member = await guild.members.fetch(user);
-                        let userHasRole = member._roles.indexOf(client.config.premiumID) >= 0;
-                        // console.log(userHasRole) false
-                        let expiredAt = await client.db.get(`premiumUser.${user}`)
-                        if (nowDateMS >= expiredAt) {
-                            await client.db.delete(`premiumUser.${user}`);
-                            member.roles.remove(premiumRole);
-                            client.logger.database(`Deleted ${member.user.tag} from premiumUser [Reason: Having expired subscription]`);
-                            let logsChannel = client.channels.cache.get(client.config.logsID);
-                            logsChannel.send({
-                                embeds: [{
-                                    title: `Premium Deletion`,
-                                    description: `Deleted **${member.user.tag}** from premiumUser \`[Reason: Having expired subscription]\``,
-                                    color: `#FF9300`,
-                                    footer: {
-                                        text: 'Powered by GrowZone',
-                                        icon_url: 'https://cdn.discordapp.com/icons/857396459392729099/a_5f4d3d9a43559fef37d5f20858fef434.gif',
-                                    }
-                                }]
-                            });
-                        }
-                    },
-                ),
-            )
-        }
-    }, 5000)
+		if (userInRole.length > 0) {
+			await Promise.all(
+				userInRole.map(async (user) => {
+					const member = await guild.members.fetch(user);
+					let subscription = await client.db.get(`premiumUser.${user}`);
+					if (!subscription || subscription === null) {
+						member.roles.remove(premiumRole);
 
+						let logsChannel = client.channels.cache.get(client.config.logsID);
+
+						logsChannel.send({
+							embeds: [
+								embed
+									.setAuthor({ name: 'Premium Status: REMOVED', iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+									.setDescription(`Premium status for **${member.user.tag}** with id \`${member.user.id}\` has been removed.`)
+									.addFields([
+										{
+											name: 'Reason',
+											value: "Provided user doesn't have any active subscription!",
+										},
+									]),
+							],
+						});
+					}
+					if (subscription <= nowDateMS) {
+						await client.db.delete(`premiumUser.${user}`);
+						member.roles.remove(premiumRole);
+						client.logger.database(`Deleted ${member.user.tag} from premiumUser [Reason: Having expired subscription]`);
+						let logsChannel = client.channels.cache.get(client.config.logsID);
+						logsChannel.send({
+							embeds: [
+								embed
+									.setAuthor({ name: 'Premium Status: ENDED', iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+									.setDescription(`Premium status for **${member.user.tag}** with id \`${member.user.id}\` has been removed.`)
+									.setFields([
+										{
+											name: 'Reason',
+											value: 'Provided user subscription has ended!',
+										},
+									]),
+							],
+						});
+					}
+				}),
+			);
+		}
+		if (userID.length > 0) {
+			await Promise.all(
+				userID.map(async (user) => {
+					const member = await guild.members.fetch(user);
+					let userHasRole = member._roles.indexOf(client.config.premiumID) >= 0;
+					// console.log(userHasRole) false
+					let expiredAt = await client.db.get(`premiumUser.${user}`);
+					if (nowDateMS >= expiredAt) {
+						await client.db.delete(`premiumUser.${user}`);
+						member.roles.remove(premiumRole);
+						client.logger.database(`Deleted ${member.user.tag} from premiumUser [Reason: Having expired subscription]`);
+						let logsChannel = client.channels.cache.get(client.config.logsID);
+						logsChannel.send({
+							embeds: [
+								embed
+									.setAuthor({ name: 'Premium Status: ENDED', iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+									.setDescription(`Premium status for **${member.user.tag}** with id \`${member.user.id}\` has been removed.`)
+									.setFields([
+										{
+											name: 'Reason',
+											value: 'Provided user subscription has ended!',
+										},
+									]),
+							],
+						});
+					}
+				}),
+			);
+		}
+	}, 5000);
 };
